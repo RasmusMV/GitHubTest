5a9d26254269944e53ab194556c0e84cd29a0fca
Rasmus Melgaard Villumsen <rav001@edu.zealand.dk>
2025-11-14 10:27:39 +0100

    Added a new class

---
+++ ConsoleApp1/RandomClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class RandomClass
+{
+    string _name;
+    string _gender;
+    public RandomClass(string name, string gender)
+	{
+        _name = name;
+        _gender = gender;
+	}
+
+    public string Name
+    {
+        get { return _name; }
+    }
+}
